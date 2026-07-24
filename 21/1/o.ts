4f4da88f0d480401787a8db69a6d637e05366fa7
piotrzdziarski <pzdziarski@protonmail.com>
2026-07-24 09:25:45 +0200

    Changes to opening the lightbox
    1. The lightbox is not trying to reopen itself when it's opened
    2. The previous lightbox closes when the sources or other props change

--- 21/1/o.ts
+++ 21/1/o.ts
@@ -1,4 +1,4 @@
-import{Component,afterEveryRender,input,inject,ElementRef,signal}from"@angular/core";import{NgComponentOutlet}from"@angular/common";var r:any,l=["onOpen","onClose","onInit","onShow","onSourceLoad","disableLocalStorage","types","type","customAttributes","maxYoutubeVideoDimensions","autoplay","autoplays","loadOnlyCurrentSource","slideDistance","openOnMount","exitFullscreenOnClose","disableSlideSwiping","disableBackgroundClose","sourceMargin","useDialog"];function rq(){r=1;require("../../../../1/1/w.js")}if(typeof window=="object")rq();function op(t:any){var j=t.sourceIndex();if(j===undefined)j=t.slide()-1;t.o.open(j||0)}
+import{Component,afterEveryRender,input,inject,ElementRef,signal}from"@angular/core";import{NgComponentOutlet}from"@angular/common";var r:any,l=["onOpen","onClose","onInit","onShow","onSourceLoad","disableLocalStorage","types","type","customAttributes","maxYoutubeVideoDimensions","autoplay","autoplays","loadOnlyCurrentSource","slideDistance","openOnMount","exitFullscreenOnClose","disableSlideSwiping","disableBackgroundClose","sourceMargin","useDialog"];function rq(){r=1;require("../../../../1/1/w.js")}if(typeof window=="object")rq();function op(t:any){if(t.o.io)return;var j=t.sourceIndex();if(j===undefined)j=t.slide()-1;t.o.open(j||0)}
 @Component({selector:"fslightbox",imports:[NgComponentOutlet],template:`@if(ob()){<div style="display:none">@for(e of s;track $index){@if(typeof e!="string"){<ng-container *ngComponentOutlet="e.component?e.component:e;inputs:e.inputs"></ng-container>}}</div>}`})
 export class FsLightbox {
 	[s: string]: any
@@ -8,8 +8,8 @@ export class FsLightbox {
 	s:any=null;
 	c:any=null;
 	constructor() {var h=inject(ElementRef);
-		afterEveryRender({mixedReadWrite:()=>{if(!r)rq();if(!this.c)return;var u,p;if(this.c.sources){if(!this.c.sources.firstChange)p=this.o.props;this.o=new(window as any).FsLightboxB();u=1;if(p)this.o.props=Object.assign({},p);this.o.props.sources=(this.sources()as any).map((e:any)=>{if(typeof e=="string")return e;var pa=h.nativeElement.firstChild,c=pa.firstElementChild;pa.removeChild(c);return c.firstChild})}this.ob.set(0);
-for(var k of l){if(this.c[k]){if(!u){p=this.o.props;this.o=new(window as any).FsLightboxB();this.o.props=Object.assign({},p);u=1}this.o.props[k]=this.c[k].currentValue}}
+		afterEveryRender({mixedReadWrite:()=>{if(!r)rq();if(!this.c)return;var u,p;if(this.c.sources){if(this.o)this.o.close();if(!this.c.sources.firstChange)p=this.o.props;this.o=new(window as any).FsLightboxB();u=1;if(p)this.o.props=Object.assign({},p);this.o.props.sources=(this.sources()as any).map((e:any)=>{if(typeof e=="string")return e;var pa=h.nativeElement.firstChild,c=pa.firstElementChild;pa.removeChild(c);return c.firstChild})}this.ob.set(0);
+for(var k of l){if(this.c[k]){if(!u){this.o.close();p=this.o.props;this.o=new(window as any).FsLightboxB();this.o.props=Object.assign({},p);u=1}this.o.props[k]=this.c[k].currentValue}}
 if((this.c.toggler&&!this.c.toggler.firstChange)||this.openOnMount())op(this);delete this.c
 		}})
 	}
